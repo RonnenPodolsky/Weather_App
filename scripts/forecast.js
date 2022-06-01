1f54bf7f4e185fe4430c2943e993d01c8b786813
ronnenn podolsky <ronnennpodolsky@ronnenns-MacBook-Pro.local>
2022-06-01 12:21:50 +0300

    finished almost

---
+++ scripts/forecast.js
@@ -0,0 +1,34 @@
+const key = 'Qs1AuzpDvNwnL45eyTrA4aoVSm1jhMIy';
+
+const getWeather = async (location_id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${location_id}?apikey=${key}`;
+    
+    const response = await fetch(base+query);
+    const data = await response.json()
+
+    return data[0];
+}
+
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+    
+    const response = await fetch(base+query);
+    const data = await response.json()
+
+    return data[0];
+}
+
+
+
+// getCity('madrid').then(data =>{
+//     return getWeather(data.Key);
+// }).then(data => {
+//     console.log(data)
+// })
+//     .catch(err => console.log(err))
+
+
+
+
